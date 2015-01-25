b213efcf1e5e169032f76427e67cc4491bcb0bf8
KRNivedha <krnivedha@gmail.com>
2015-01-25 10:54:10 -0800

    updated cachematrix.R and read.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix fn takes as input a matrix and caches 
+## the long computations of inverse & returns a list to the next fn.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) {   
+  
+  m <- NULL # the result of inversion stored here
+  # A setter fn,to set a matrix to the object created by makeCacheMatrix fn
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x # return the input matrix
+  setinv <- function(inv=matrix()) m <<- inv # set the inversed matrix
+  getinv <- function() m # return the inversed matrix
+  
+  # return a list that contains these functions,so that we could do 
+  # flist<-makeCacheMatrix(test)
+  # flist$set(new)
+  # flist$get
+  # flist$setinv
+  # flist$getinv
+  # where new,test are matrices
+  list(set=set,get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+##cacheSolve checks for the cached data otherwise computes and returns the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv() #get the inversed matrix from 'x'
+  #it ll be null if uncalculated
+  if(!is.null(m)) { #if the inversion result exists
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get() # if not,we do x$get to get the matrix object
+  m <- solve(data) # we solve it
+  x$setinv(m) # we the set it to the object
+  m # return the result
+
 }
+
+# Test
+x<-matrix(1:4,2,2)
+flist$set(matrix(c(4,16,4,8),nrow=2,ncol=2))
+#generate the makeCacheMatrix object with x
+flist<-makeCacheMatrix(x)
+# calculating the inverse and retrieving them
+cacheSolve(flist)
+cacheSolve(flist)
---
+++ read.md
@@ -0,0 +1,6 @@
+## Assignment 
+This assignment is about the concepts of caching
+Computing inverse is a long process.
+If the contents of a vector are not changing, it may make
+sense to cache the inverse of the given matrix so that when we need it again, it
+can be looked up in the cache rather than recomputed.
